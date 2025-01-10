3e2842b4ec85cf5749e2cd7127385c4730408b76
Muhammed Salman <100601161+MhmdSalman18@users.noreply.github.com>
2025-01-10 19:40:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from dataclasses import dataclass, asdict, field  # Import dataclass for creatin
 import pandas as pd  # Import pandas for handling data and saving it to files
 import argparse  # Import argparse for command-line argument parsing
 import os  # Import os for file and directory handling
-import sys  # Import sys 
+import sys  # Import 
 # and functions 
 # Data class to 
 # and functions 
